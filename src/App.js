f6f0ebe77d5cd3716153b7602c9e687b787aa2ae
Joseph DiPeri <jdiperi88@gmail.com>
2017-11-21 19:21:25 -0500

    added recipeSearch component and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import SearchRecipes from './components/SearchRecipes';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> Recipe Now!!!</h1>
+        <SearchRecipes/>
       </div>
     );
   }
