9ec75ae12768f979d997a346d0cf2495060d0bb4
Joseph DiPeri <jdiperi88@gmail.com>
2017-11-21 22:33:18 -0500

    added search function and fetch request to searchrecipe component

--- src/components/SearchRecipes.js
+++ src/components/SearchRecipes.js
@@ -2,22 +2,50 @@ import React,{Component} from 'react';
 import { Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap'
 
 class SearchRecipes extends Component{
+    constructor(){
+        super();
+        this.state = {
+            ingredients : '',
+            dish: ''
+        }
+    }
+    search(){
+        let {ingredients, dish } = this.state
+        const url = `http://www.recipepuppy.com/api?i=${ingredients}&q=${dish}`
+        console.log(this.state, url)
+        fetch(url,{
+            method: 'GET'
+        })
+        .then(res => {
+            res.json()
+        .then(json => console.log(json))
+        })
+    }
     render(){
         return(
             <Form inline>
                 <FormGroup>
                     <ControlLabel >Ingredients </ControlLabel>
                     {' '}
-                    <FormControl type="text" placeholder="garlic, chicken"/>
+                    <FormControl 
+                        type="text" 
+                        placeholder="garlic, chicken"
+                        onChange ={e => this.setState({ingredients: e.target.value })}
+                    
+                    />
                 </FormGroup>
                 {' '}
                 <FormGroup>
                     <ControlLabel> Dish </ControlLabel>
                     {' '}
-                    <FormControl type="text" placeholder="Adobo"/>
+                    <FormControl 
+                        type="text" 
+                        placeholder="Adobo"
+                        onChange= {e => this.setState({ dish: e.target.value }) }
+                    />
                 </FormGroup>
                 {' '}
-                <Button>Submit</Button>
+                <Button onClick={ () =>this.search()}>Submit</Button>
             </Form>
         )
     }
