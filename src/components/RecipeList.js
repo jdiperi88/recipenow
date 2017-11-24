2dfc2fca0f0a687b2ce9aea1706578e213f5ebd0
Joseph DiPeri <jdiperi88@gmail.com>
2017-11-24 11:58:28 -0500

    added home links and conditional logic on favorite recipe links

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import RecipeItem  from './RecipeItem';
+import { favoriteRecipe } from '../actions/index';
 
 class RecipeList extends Component{
     render(){
         return (
         <div>
-            <h4><Link to='/favorites'>Favorites</Link></h4>
+            {
+                this.props.favoriteRecipes.length > 0 
+                ?
+                <h4 className='link'><Link to='/favorites'>Favorites</Link></h4>
+                :
+                <div></div>
+            }
+            
             {
                 this.props.recipes.map((recipe,i) =>{
                     return (
